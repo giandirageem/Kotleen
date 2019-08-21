49a2551a70ad37a1ee0181453d216fc59dbc3e83
Giandira Gumelar Maulana <38846864+giandirageem@users.noreply.github.com>
2019-08-21 17:06:23 +0700

    Add files via upload
    Numeric in kotlin

---
+++ Numerik.kt
@@ -0,0 +1,13 @@
+val nInteger = 240
+val nLong = 2000L
+val nShort : Short = 20
+val nByte = 0b11010011
+val nDouble = 2.800
+
+fun main() {
+	println("Nilai Integer : $nInteger")
+	println("Nilai Long : $nLong")
+	println("Nilai Short : $nShort")
+	println("Nilai Byte : $nByte")
+	println("Nilai Double : $nDouble")
+}
